5d0759a28670e421e8914d2246e169889da79a6f
Alexis Ojeda <aojeda@JKLI.COM>
2022-08-01 16:12:26 -0400

    Add notes.js, use props and map to generate notes using notes array, and convert functions to arrow functions.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(noteItem => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} /> )}
             <Footer/>
         </div>
     );
