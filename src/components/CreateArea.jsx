b95ae2efbf4eb8f8ab1bb3dc13bb579d6a79e162
Alexis Ojeda <aojeda@JKLI.COM>
2022-09-07 15:17:11 -0400

    Add material icons. Add background image.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
 
   const [note, setNote] = useState({
     title: "",
@@ -27,23 +32,36 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input 
-          name="title"
-          onChange={handleChagne}
-          value={note.title} 
-          placeholder="Title" 
-        />
+        {isExpanded && (
+          <input 
+            name="title"
+            onChange={handleChagne}
+            value={note.title} 
+            placeholder="Title" 
+          />
+        )}
+
         <textarea 
           name="content" 
+          onClick={expand}
           onChange={handleChagne}
           value={note.content} 
           placeholder="Take a note..." 
-          rows="3" 
+          rows={isExpanded ? 3 : 1} 
         />
-        <button onClick={submitNote}>Add</button>
+
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
